0f0ffbb5685a61afcc071ad39fe44ee9346a8985
Eduardo Função <eduardofuncao@hotmail.com>
2025-06-01 18:22:00 -0300

    first commit from spring initializr

---
+++ src/main/java/br/com/fiap/chents/ChentsApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.chents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/chents/ChentsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.chents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
